09af2d45ccc659e417e54b100a1c4a618752e93c
labixiaoK <shaokelv@163.com>
2017-08-16 12:30:02 +0800

    Add files via upload

---
+++ assignment1/q2_neural.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+import numpy as np
+import random
+
+from q1_softmax import softmax
+from q2_sigmoid import sigmoid, sigmoid_grad
+from q2_gradcheck import gradcheck_naive
+
+
+def forward_backward_prop(data, labels, params, dimensions):
+    """
+    Forward and backward propagation for a two-layer sigmoidal network
+
+    Compute the forward propagation and for the cross entropy cost,
+    and backward propagation for the gradients for all parameters.
+
+    Arguments:
+    data -- M x Dx matrix, where each row is a training example.
+    labels -- M x Dy matrix, where each row is a one-hot vector.
+    params -- Model parameters, these are unpacked for you.
+    dimensions -- A tuple of input dimension, number of hidden units
+                  and output dimension
+    """
+
+    ### Unpack network parameters (do not modify)
+    ofs = 0
+    Dx, H, Dy = (dimensions[0], dimensions[1], dimensions[2])
+
+    W1 = np.reshape(params[ofs:ofs+ Dx * H], (Dx, H))
+    ofs += Dx * H
+    b1 = np.reshape(params[ofs:ofs + H], (1, H))
+    ofs += H
+    W2 = np.reshape(params[ofs:ofs + H * Dy], (H, Dy))
+    ofs += H * Dy
+    b2 = np.reshape(params[ofs:ofs + Dy], (1, Dy))
+
+    #forward propagation
+    h = sigmoid(np.dot(data, W1) + b1)
+    yhat = softmax(np.dot(h, W2) + b2)
+    
+    #backward propagation
+    cost = np.sum(-np.log(yhat[labels==1])) / data.shape[0]
+    d3 = (yhat - labels) / data.shape[0]
+    gradW2 = np.dot(h.T, d3) 
+    gradb2 = np.sum(d3, axis=0, keepdims=True) 
+    
+    dh = np.dot(d3, W2.T)
+    grad_h = dh * sigmoid_grad(h) 
+    
+    gradW1 = np.dot(data.T, grad_h)
+    gradb1 = np.sum(grad_h, axis=0, keepdims=True)
+ 
+    ### Stack gradients (do not modify)
+    grad = np.concatenate((gradW1.flatten(), gradb1.flatten(),
+        gradW2.flatten(), gradb2.flatten()))
+
+    return cost, grad
+
+
+def sanity_check():
+    """
+    Set up fake data and parameters for the neural network, and test using
+    gradcheck.
+    """
+    print ("Running sanity check...")
+
+    N = 20
+    dimensions = [10, 5, 10]
+    data = np.random.randn(N, dimensions[0])   # each row will be a datum
+    labels = np.zeros((N, dimensions[2]))
+    for i in xrange(N):
+        labels[i, random.randint(0,dimensions[2]-1)] = 1
+
+    params = np.random.randn((dimensions[0] + 1) * dimensions[1] + (
+        dimensions[1] + 1) * dimensions[2], )
+
+    gradcheck_naive(lambda params:
+        forward_backward_prop(data, labels, params, dimensions), params)
+
+
+
+
+if __name__ == "__main__":
+    sanity_check()
+
